aefe0de766afdc809f00c6c6b98b98423da34951
Ambassador382 <113097996+Ambassador382@users.noreply.github.com>
2022-09-08 17:09:12 +0700

    Add documentation v1

---
+++ DocumentationLab1/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"chigvintsevns.ru.pmu_lab1"}]
---
+++ DocumentationLab1/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"chigvintsevns.ru.pmu_lab1","l":"ExampleInstrumentedTest"},{"p":"chigvintsevns.ru.pmu_lab1","l":"ExampleUnitTest"},{"p":"chigvintsevns.ru.pmu_lab1","l":"MainActivity"},{"p":"chigvintsevns.ru.pmu_lab1","l":"MinMax"}]
