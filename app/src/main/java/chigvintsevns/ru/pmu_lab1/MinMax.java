d5bcc355076cb5cd8c0f380f6258aa2821f95da5
Ambassador382 <nikmail007@gmail.com>
2022-09-08 16:08:06 +0700

    Added MinMax class and simple unit tests

---
+++ app/src/main/java/chigvintsevns/ru/pmu_lab1/MinMax.java
@@ -0,0 +1,14 @@
+package chigvintsevns.ru.a2lab1;
+
+public class MinMax {
+    public int min2(int a, int b) {
+        if (a < b)
+            return a;
+        return b;
+    }
+    public int max2(int a, int b) {
+        if (a > b)
+            return a;
+        return b;
+    }
+}
--- app/src/test/java/chigvintsevns/ru/pmu_lab1/ExampleUnitTest.java
+++ app/src/test/java/chigvintsevns/ru/pmu_lab1/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package chigvintsevns.ru.pmu_lab1;
+package chigvintsevns.ru.a2lab1;
 
 import org.junit.Test;
 
@@ -10,8 +10,57 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+    chigvintsevns.ru.a2lab1.MinMax minMax = new chigvintsevns.ru.a2lab1.MinMax();
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void minTest1() {
+        //arr
+        int[] result = new int[1];
+        int[] expected = new int[1];
+        expected[0] = 1;
+        int a = 1;
+        int b = 2;
+        //act
+        result[0] = minMax.min2(a, b);
+        //assert
+        assertArrayEquals(expected, result);
+    }
+    @Test
+    public void minTest2() {
+        //arr
+        int[] result = new int[1];
+        int[] expected = new int[1];
+        expected[0] = 1;
+        int a = 2;
+        int b = 1;
+        //act
+        result[0] = minMax.min2(a, b);
+        //assert
+        assertArrayEquals(expected, result);
+    }
+    @Test
+    public void maxTest1() {
+        //arr
+        int[] result = new int[1];
+        int[] expected = new int[1];
+        expected[0] = 2;
+        int a = 1;
+        int b = 2;
+        //act
+        result[0] = minMax.max2(a, b);
+        //assert
+        assertArrayEquals(expected, result);
+    }
+    @Test
+    public void maxTest2() {
+        //arr
+        int[] result = new int[1];
+        int[] expected = new int[1];
+        expected[0] = 2;
+        int a = 2;
+        int b = 1;
+        //act
+        result[0] = minMax.max2(a, b);
+        //assert
+        assertArrayEquals(expected, result);
     }
 }
